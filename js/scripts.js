5b1bc38caa5edbfb1cfee032697df4edb1204773
nishuvo <nishuvo@outlook.com>
2019-08-07 14:29:30 +0600

    temp web

---
+++ js/scripts.js
@@ -0,0 +1,61 @@
+// Top primary navigation 
+// navigation menu
+$(document).ready(function () {
+    var scrollTop = 0;
+    $(window).scroll(function () {
+        scrollTop = $(window).scrollTop();
+        $('.counter').html(scrollTop);
+
+        if (scrollTop >= 100) {
+            $('.bg-transparent').addClass('scrolled-nav');
+        } else if (scrollTop < 100) {
+            $('.bg-transparent').removeClass('scrolled-nav');
+        }
+
+    });
+
+});
+// autohide on click
+$('.navbar-nav>li>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+});
+
+// contact form actions (require jQuery validator)
+$("#contactform").validate({
+    rules: {
+      name: {
+        required: true
+      },
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    submitHandler: function(form) {
+      var form = $("#contactform");
+      var submitButton = $("#submit");
+      var response = $('#response');
+      $.ajax({
+        method: "POST",
+        url: "https://www.briskforms.com/go/64761793842c44fc6bdc3ed96589d101",
+        data: $("#contactform").serialize(),
+        dataType: "json",
+  
+        beforeSend: function() {
+          response.text('Sending....');
+        },
+        success: function(data) {
+          response.html('<i class="fas fa-check"></i> Message sent').fadeIn();
+          form.trigger('reset');
+        },
+        error: function(e) {
+          console.log(e);
+          response.html('<i class="fas fa-check"></i> Message sent').fadeIn();
+        }
+      });
+    }
+  });
+
+// get the year
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
